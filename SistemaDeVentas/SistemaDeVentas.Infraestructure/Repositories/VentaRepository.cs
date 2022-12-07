57b7aac7b1ae754c37be3986ce6fa9373a98ec9b
Facundo-Romani <facundoromani90@gmail.com>
2022-12-06 21:11:53 -0300

    Implemetacion Venta

---
+++ SistemaDeVentas/SistemaDeVentas.Infraestructure/Repositories/VentaRepository.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SistemaDeVentas.DomainEntities.Entities;
+using SistemaDeVentas.Infraestructure.Context;
+using SistemaDeVentas.Infraestructure.RepositoriesContracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaDeVentas.Infraestructure.Repositories
+{
+    public class VentaRepository : IVentaRepository
+    {
+        private readonly DbContextSistema _context;
+
+        public VentaRepository(DbContextSistema context)
+        {
+            _context = context;
+        }
+
+        public async Task<ICollection<Venta>> GetAllAsync()
+        {
+            return await _context.Ventas.ToListAsync();
+        }
+
+        public async Task<Venta> GetIdAsync(int id)
+        {
+            Venta? venta = await _context.Ventas.FirstOrDefaultAsync(v => v.Id == id);
+
+            return venta;
+        }
+    }
+}
---
+++ SistemaDeVentas/SistemaDeVentas.Infraestructure/RepositoriesContracts/IVentaRepository.cs
@@ -0,0 +1,16 @@
+﻿using SistemaDeVentas.DomainEntities.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaDeVentas.Infraestructure.RepositoriesContracts
+{
+    public interface IVentaRepository
+    {
+        Task<ICollection<Venta>> GetAllAsync();
+
+        Task<Venta> GetIdAsync(int id);
+    }
+}
